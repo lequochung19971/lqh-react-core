7ccd7ea0bff4088bac45f98d73b987ff8aeac4ef
hung.lequoc <hung.lequoc@hdwebsoft.co>
2021-04-18 09:47:41 +0700

    update validation config

--- src/app/AppInit.ts
+++ src/app/AppInit.ts
@@ -1,5 +1,5 @@
 import { HttpInit } from '@http';
-import { YupAddMethods } from '@shared/utils/YupAddMethods';
+import { YupAddMethods } from '@shared/validations/YupAddMethods';
 import { allYupMethods } from '@shared/validations';
 
 export default function AppInit(): void {
