29a633f86e0977b4f1afa203148ffb4b6aad77a0
hung.lequoc <hung.lequoc@hdwebsoft.co>
2021-06-27 22:53:33 +0700

    update

--- src/app/AppInit.ts
+++ src/app/AppInit.ts
@@ -1,10 +1,10 @@
 import { HttpInit } from '@http';
-import { allYupMethods } from '@shared/utils/yup';
-import { yupAddMethods } from '@shared/utils/yup/yupAddMethods';
+// import { allYupMethods } from '@shared/utils/yup';
+// import { yupAddMethods } from '@shared/utils/yup/yupAddMethods';
 
 export default function AppInit(): void {
   HttpInit();
 
 	// For using Yup to handle validation. If not, you can remove.
-	yupAddMethods(allYupMethods);
+	// yupAddMethods(allYupMethods);
 }
